bcad181d06899fcca7e2b7c62a5778730bd136c4
Andrey <chinoaj_96@hotmail.com>
2019-09-23 04:02:37 -0600

    loadjson added

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,14 @@ function main() {
       return;
     }
 
-    
+    if (isInLocalStorage()) {
+      displayResultInfo();
+    } else if (isInDataApi()) {
+      storeInLocalStorage();
+      displayResultInfo();
+    }
+
+    // displayEmailNotFound();
 
   }
 
@@ -21,7 +28,6 @@ function main() {
   // This function will return false if the email input value is not valid
   function checkEmail() {
     let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
-
     if (!emailInput.value.match(mailFormat)) {
       return false;
     }
@@ -31,13 +37,54 @@ function main() {
 
   // This function will display an error message to the user
   function displayEmailErrorMessage() {
-
     emailInput.value = "";
     emailInput.placeholder = "Please add a valid email address";
     emailInput.classList.add("error");
-
   }
   // End of showErrorMessage function
+
+  // This function will check if any data or the specific email is already in local storage
+  function isInLocalStorage() {
+
+    let data = JSON.parse(localStorage.getItem('reports'));
+    if (!data) {
+      return false;
+    }
+    if (!data.reports.some(report => report.email === emailInput.value)) {
+      return false;
+    }
+    return true;
+  }
+  // End of isInLocalStorage function
+
+  // This function will check if the email information is in data.json
+  function isInDataApi() {
+
+    loadJSON(function(response){
+
+      let data = JSON.parse(response);
+      console.log(data);
+
+    });
+
+  }
+  // End of isInDataApi function
+
+  //
+  function loadJSON(callback) {
+    let xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+
+    xobj.open('GET', '../data/data.json', true);
+    xobj.onreadystatechange = function () {
+      if (xobj.readyState == 4 && xobj.status == "200") {
+        callback(xobj.responseText);
+      }
+    };
+
+    xobj.send(null);
+  }
+  //
 
 }
 
